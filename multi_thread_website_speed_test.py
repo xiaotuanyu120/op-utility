46e831c36b97c2bcc96269f427332ce935e2b758
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-03-03 19:52:28 -0500

    finish url class optimize

--- multi_thread_website_speed_test.py
+++ multi_thread_website_speed_test.py
@@ -19,40 +19,50 @@ class UrlHandler(object):
                 self.url_list.append(line[:-1])
         return self.url_list 
 
-    def urlopen(self, test_url)
+    def url_open(self, url):
+        return urllib2.urlopen(url)
 
-    def test(self, test_url):
-        setup_str = "\
-            import urllib2
-            
-"
-        url_test_t = timeit.Timer('urllib2.urlopen(test_url)', setup=setup_str)
+
+class UrlTest(object):
+    def __init__(self, test_url):
+        self.test_url = test_url
+
+    def test_access_time(self):
+        setup_str = '''\
+from __main__ import UrlHandler
+url_h = UrlHandler()
+test_url = "%s"''' % self.test_url
+        url_test_t = timeit.Timer('url_h.url_open(test_url)', setup=setup_str)
         return url_test_t.timeit(1)
 
 
-class TestThread(threading.Thread):
-    def __init__(self, threadID, test_q):
+class ThreadControl(threading.Thread):
+
+    def __init__(self, test_q):
         threading.Thread.__init__(self)
-        self.threadID = threadID
         self.q = test_q
 
     def run(self):
         while not exitFlag:
             queue_lock.acquire()
-            if not work_queue.empty():
+            if not self.q.empty():
                 url = self.q.get()
-                url_access_time = UrlHandler().test(url)
+                url_access_time = UrlTest(url).test_access_time()
                 print "%-40s%.2fs" % (url, url_access_time)
                 queue_lock.release()
             else:
                 queue_lock.release()
+        return
+
+    def queue_prepare(self):
+        return
 
 
 url_file_path = "./urllist"
 url_list = UrlHandler().make_list(url_file_path)
 exitFlag = 0
 queue_lock = threading.Lock()
-work_queue = Queue.Queue()
+work_q = Queue.Queue()
 threads = []
 threadID = 1
 
@@ -60,16 +70,17 @@ threadID = 1
 if __name__ == "__main__":
     queue_lock.acquire()
     for url in url_list:
-        work_queue.put(url)
+        work_q.put(url)
     queue_lock.release()
 
     while threadID <= 5:
-        thread = TestThread(threadID, work_queue)
+        thread = ThreadControl(work_q)
         thread.start()
+        print "thread: %s is ready" % threadID
         threads.append(thread)
         threadID += 1
 
-    while not work_queue.empty():
+    while not work_q.empty():
         pass 
 
     exitFlag = 1
