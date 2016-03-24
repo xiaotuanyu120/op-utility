d2f9d9e80eca86a876b7a3e6bb2d1d6abb8c82d9
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-03-24 04:19:28 -0400

    hide some interal fuction by use 'import ** as _**'

--- ssh_key_dispense/main.py
+++ ssh_key_dispense/main.py
@@ -2,8 +2,8 @@ from fabric.api import local
 from fabric.api import env
 from fabric.api import runs_once
 
-from keygen_ssh import key_gen
-from keygen_ssh import key_copy
+from keygen_ssh import key_gen as _key_gen
+from keygen_ssh import key_copy as _key_copy
 
 hosts = {
         '172.16.2.3': 'Sudoroot88',
@@ -16,9 +16,9 @@ env.passwords = {'root@'+x+':22': hosts[x] for x in hosts}
 @runs_once
 def ssh_key_gen():
     for host in hosts:
-        key_gen(host)
+        _key_gen(host)
 
 @runs_once
 def ssh_key_copy():
     for host in hosts:
-        key_copy(host, hosts[host])
+        _key_copy(host, hosts[host])
