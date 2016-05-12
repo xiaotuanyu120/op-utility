368146a8f4463e7268bd58e7d237d213d04e1dbc
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-05-13 07:55:28 +0800

    mysql-install.sh optimized

--- lnmp-install/mysql-install.sh
+++ lnmp-install/mysql-install.sh
@@ -1,39 +1,47 @@
-echo 'mysql base env install'
+## env setting
+BASEDIR=/usr/local/mysql
+DATADIR=/data/mysql-data
+PASSWORD=igamemysql
+
+## mysql base env install
 yum install gcc gcc-c++ cmake ncurses-devel -y
 yum groupinstall base "Development Tools" -y
 
-echo 'create user'
+## create user
 groupadd mysql
 useradd -r -g mysql mysql
 
-echo 'source package download'
-cd /usr/local/src
-wget http://cdn.mysql.com/archives/mysql-5.1/mysql-5.1.72.tar.gz
-tar zxvf mysql-5.1.72.tar.gz
-cd mysql-5.1.72
+## source package unzip
+[[ -d mysql-5.1.72 ]] || mkdir mysql && mv mysql /tmp && tmp_mv=1
+tar zxvf mysql-5.1.72.tar.gz -C mysql
+cd mysql
 
-echo 'mysql install'
-./configure --prefix=/usr/local/mysql --with-mysqld-user=mysql --with-charset=utf8 --with-extra-charsets=all
+## mysql install
+./configure --prefix=$BASEDIR --datadir=$DATADIR --with-mysqld-user=mysql --with-charset=utf8 --with-extra-charsets=all
 make
 make install
 
 mkdir -p /data/mysql-data
-chown -R mysql:mysql /usr/local/mysql/
-chown -R mysql:mysql /data/mysql-data/
+chown -R mysql:mysql $BASEDIR
+chown -R mysql:mysql $DATADIR
 
-echo 'mysql initial'
-./scripts/mysql_install_db --datadir=/data/mysql-data/ --basedir=/usr/local/mysql/ --user=mysql
+## mysql initial
+./scripts/mysql_install_db --datadir=$DATADIR --basedir=$BASEDIR --user=mysql
 cp ./support-files/mysql.server /etc/init.d/mysqld
 mv /etc/my.cnf /etc/my.cnf.old
 cp support-files/my-medium.cnf /etc/my.cnf
 chmod 755 /etc/init.d/mysqld
 
-sed -inr 's#^basedir=#basedir=/usr/local/mysql#g' /etc/init.d/mysqld
-sed -inr 's#^datadir=#datadir=/data/mysql-data#g' /etc/init.d/mysqld
+sed -inr "s#^basedir=#basedir=$(BASEDIR)#g" /etc/init.d/mysqld
+sed -inr "s#^datadir=#datadir=$(DATADIR)#g" /etc/init.d/mysqld
 
+## service start and enanble
 chkconfig mysqld on
 /etc/init.d/mysqld start
-/usr/local/mysql/bin/mysqladmin -u root password 'igamemysql'
+/usr/local/mysql/bin/mysqladmin -u root password "$(PASSWORD)"
 
-echo "export PATH=$PATH:/usr/local/mysql/bin" >> /etc/profile
+echo "export PATH=$PATH:$(BASEDIR)/bin" >> /etc/profile
 . /etc/profile
+
+## copy file moved to tmp back
+[[ tmp_mv ]] && mv ./mysql /tmp/mysql_source && mv /tmp/mysql .
