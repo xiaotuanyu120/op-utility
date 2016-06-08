7b1a5e19fe74277cd79f2561822caac6886740d1
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-06-08 19:14:39 +0800

    main bug fix

--- access_ip_sum_main.sh
+++ access_ip_sum_main.sh
@@ -2,6 +2,7 @@
 # use access_ip_sum.py to analysis access ip count
 
 LOGPATH=/web/www
+ANALYSIS=$LOGPATH/analysis
 
-mkdir -p LOGPATH/analysis
-ls ${LOGPATH}/*.log|xargs -i python access_ip_sum.py {} {}/analysis.analysis
+mv $ANALYSIS ${ANALYSIS}-`data +%F%H:%M:%S`
+ls ${LOGPATH}/*.log|xargs -i python access_ip_sum.py {} {}.analysis
