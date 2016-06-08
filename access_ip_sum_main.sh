5caa217d64eeea98e2faec5748b50545c2e2286e
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-06-08 19:30:24 +0800

    main bug fix

--- access_ip_sum_main.sh
+++ access_ip_sum_main.sh
@@ -4,7 +4,7 @@
 LOGPATH=/web/log
 ANALYSIS=$LOGPATH/analysis
 
-mv $ANALYSIS ${ANALYSIS}-`date +%F%H:%M:%S`
+mv $ANALYSIS ${ANALYSIS}-`date +%F%H:%M:%S` 2>/dev/null
 mkdir $ANALYSIS
 ls ${LOGPATH}/*.log|xargs -i python access_ip_sum.py {} {}.analysis
 mv $LOGPATH/*.analysis $ANALYSIS
