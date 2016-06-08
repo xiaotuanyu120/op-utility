54ce3f9cb323ec3f597ed3eae52258cde59ae6d3
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-06-08 19:19:24 +0800

    main bug fix

--- access_ip_sum_main.sh
+++ access_ip_sum_main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # use access_ip_sum.py to analysis access ip count
 
-LOGPATH=/web/www
+LOGPATH=/web/log
 ANALYSIS=$LOGPATH/analysis
 
+mkdir $ANALYSIS
 mv $ANALYSIS ${ANALYSIS}-`data +%F%H:%M:%S`
 ls ${LOGPATH}/*.log|xargs -i python access_ip_sum.py {} {}.analysis
+mv $LOGPATH/*.analysis $ANALYSIS
